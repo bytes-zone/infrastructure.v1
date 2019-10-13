7a346a215d8cff574e53551d46af0332a14d29ce
Brian Hicks <brian@brianthicks.com>
2019-10-13 13:42:20 -0500

    add deploy script

---
+++ gitea/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+HOST="${1:-}"
+STORE_PATH="${2:-}"
+if test -z "$HOST" || test -z "$STORE_PATH"; then
+  echo "Usage: ${0} HOST STORE_PATH"
+  exit 1
+fi
+STORE_PATH="$(realpath "$STORE_PATH")"
+
+set -x
+nix-copy-closure --to --use-substitutes "$HOST" "$STORE_PATH"
+ssh "$HOST" -- "sudo nix-env --profile /nix/var/nix/profiles/system --set ${STORE_PATH}"
+ssh "$HOST" -- "sudo /nix/var/nix/profiles/system/bin/switch-to-configuration switch"
