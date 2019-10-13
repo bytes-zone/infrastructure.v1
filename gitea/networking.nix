2daef35fb338001592bcb87a72bacdd104670d52
Brian Hicks <brian@brianthicks.com>
2019-10-13 14:00:34 -0500

    update with new droplet

--- gitea/networking.nix
+++ gitea/networking.nix
@@ -2,16 +2,16 @@
   # This file was populated at runtime with the networking
   # details gathered from the active system.
   networking = {
-    nameservers = [ "67.207.67.3" "67.207.67.2" ];
-    defaultGateway = "157.245.128.1";
-    defaultGateway6 = "2604:a880:400:d0::1";
+    nameservers = [ "67.207.67.2" "67.207.67.3" ];
+    defaultGateway = "167.99.144.1";
+    defaultGateway6 = "2604:a880:400:d1::1";
     dhcpcd.enable = false;
     usePredictableInterfaceNames = lib.mkForce true;
     interfaces = {
       eth0 = {
         ipv4.addresses = [
           {
-            address = "157.245.134.107";
+            address = "167.99.156.113";
             prefixLength = 20;
           }
           {
@@ -21,20 +21,20 @@
         ];
         ipv6.addresses = [
           {
-            address = "2604:a880:400:d0::4a9c:a001";
+            address = "2604:a880:400:d1::ab1:1";
             prefixLength = 64;
           }
           {
-            address = "fe80::1ccc:a5ff:fede:6281";
+            address = "fe80::a09a:5eff:fee5:1c37";
             prefixLength = 64;
           }
         ];
         ipv4.routes = [{
-          address = "157.245.128.1";
+          address = "167.99.144.1";
           prefixLength = 32;
         }];
         ipv6.routes = [{
-          address = "2604:a880:400:d0::1";
+          address = "2604:a880:400:d1::1";
           prefixLength = 32;
         }];
       };
@@ -42,7 +42,7 @@
     };
   };
   services.udev.extraRules = ''
-    ATTR{address}=="1e:cc:a5:de:62:81", NAME="eth0"
+    ATTR{address}=="a2:9a:5e:e5:1c:37", NAME="eth0"
 
   '';
 }
