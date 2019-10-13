40086f5de196331c7740e12c788017c19d9dae8a
Brian Hicks <brian@brianthicks.com>
2019-10-13 13:42:01 -0500

    mount disks

--- gitea/hardware-configuration.nix
+++ gitea/hardware-configuration.nix
@@ -1,8 +1,20 @@
 { ... }: {
   imports = [ <nixpkgs/nixos/modules/profiles/qemu-guest.nix> ];
   boot.loader.grub.device = "/dev/vda";
-  fileSystems."/" = {
-    device = "/dev/vda1";
-    fsType = "ext4";
+  fileSystems = {
+    "/" = {
+      device = "/dev/vda1";
+      fsType = "ext4";
+    };
+    "/mnt/db" = {
+      device = "/dev/disk/by-label/db";
+      fsType = "ext4";
+      options = [ "discard" "defaults" "noatime" ];
+    };
+    "/mnt/objects" = {
+      device = "/dev/disk/by-label/objects";
+      fsType = "ext4";
+      options = [ "discard" "defaults" "noatime" ];
+    };
   };
 }
