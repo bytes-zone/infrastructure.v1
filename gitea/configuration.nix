078342c7aac05784506f1137ea3ffc568e318669
Brian Hicks <brian@brianthicks.com>
2020-12-02 09:20:19 -0600

    fix problems with CSP

--- gitea/configuration.nix
+++ gitea/configuration.nix
@@ -191,7 +191,7 @@ in {
 
       extraConfig = ''
         add_header Strict-Transport-Security max-age=15768000 always;
-        add_header Content-Security-Policy "default-src 'self'; object-src 'none'; require-trusted-types-for 'script'" always;
+        add_header Content-Security-Policy "default-src 'none'; child-src https:; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:" always;
         add_header X-Frame-Options "SAMEORIGIN" always;
         add_header X-Content-Type-Options "nosniff" always;
         add_header X-XSS-Protection "1; mode=block" always;
