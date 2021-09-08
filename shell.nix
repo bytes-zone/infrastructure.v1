4f7aadda81d0a819016fb851acbd9348b1352bc5
Brian Hicks <brian@brianthicks.com>
2021-09-07 20:44:34 -0500

    drop tarsnap

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
 { ... }:
 let
   sources = import ./nix/sources.nix;
-  nixpkgs = import sources.nixpkgs { config = { allowUnfree = true; }; };
+  nixpkgs = import sources.nixpkgs { };
   niv = import sources.niv { };
 in with nixpkgs;
 stdenv.mkDerivation {
   name = "gitea-experiment";
-  buildInputs = [ niv.niv git terraform graphviz tarsnap ];
+  buildInputs = [ niv.niv git terraform graphviz ];
 }
