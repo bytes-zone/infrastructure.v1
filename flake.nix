72865ca4a034ea2b43c46fcada9352000c94b375
Brian Hicks <brian@brianthicks.com>
2021-09-13 16:12:26 -0500

    start a new flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "bytes.zone infrastructure";
+
+  inputs = {
+    nixpkgs-release.url = "github:NixOS/nixpkgs/release-21.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    bad-datalog = {
+      url = "git+https://git.bytes.zone/brian/bad-datalog.git?ref=main";
+      flake = false;
+    };
+
+    bytes-zone = {
+      url = "git+https://git.bytes.zone/bytes.zone/bytes.zone.git?ref=main";
+      flake = false;
+    };
+
+    comma = {
+      url = "github:Shopify/comma";
+      flake = false;
+    };
+
+    elo-anything = {
+      url = "git+https://git.bytes.zone/brian/elo-anything.git?ref=main";
+      flake = false;
+    };
+
+    goatcounter = {
+      url = "github:zgoat/goatcounter/release-1.4";
+      flake = false;
+    };
+  };
+
+  outputs = inputs: { };
+}
