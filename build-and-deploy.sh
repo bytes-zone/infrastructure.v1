4b6c85e304ff9b0d5d6cbd589cbd05c147ce0a9c
Brian Hicks <brian@brianthicks.com>
2021-09-14 08:00:33 -0500

    add a combo build-and-deploy script

---
+++ build-and-deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+BUILD_TARGET="${1:-}"
+DEPLOY_TARGET="${2:-}"
+
+if test -z "$BUILD_TARGET" || test -z "$DEPLOY_TARGET"; then
+  echo "USAGE: ${0:-} BUILD_TARGET DEPLOY_TARGET"
+  exit 1
+fi
+
+./build.sh "$BUILD_TARGET"
+./deploy.sh "$DEPLOY_TARGET" result
