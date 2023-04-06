e84d23ecc20e6c4b70057c229beb31fc968fe3d9
Brian Hicks <brian@brianthicks.com>
2023-04-06 16:53:39 -0500

    add the script from the nixos wiki for upgrading a db

--- machines/gitea/default.nix
+++ machines/gitea/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }: {
   imports = [
     ./hardware-configuration.nix
     ./networking.nix # generated at runtime by nixos-infect
@@ -30,6 +30,30 @@
     pkgs.goatcounter
     pkgs.kakoune-unwrapped
     pkgs.sysz
+    (
+      let newPostgres = pkgs.postgresql_15;
+      in pkgs.writeScriptBin "upgrade-pg-cluster" ''
+        set -eux
+        # XXX it's perhaps advisable to stop all services that depend on postgresql
+        systemctl stop postgresql
+
+        export NEWDATA="/var/lib/postgresql/${newPostgres.psqlSchema}"
+
+        export NEWBIN="${newPostgres}/bin"
+
+        export OLDDATA="${config.services.postgresql.dataDir}"
+        export OLDBIN="${config.services.postgresql.package}/bin"
+
+        install -d -m 0700 -o postgres -g postgres "$NEWDATA"
+        cd "$NEWDATA"
+        sudo -u postgres $NEWBIN/initdb -D "$NEWDATA"
+
+        sudo -u postgres $NEWBIN/pg_upgrade \
+          --old-datadir "$OLDDATA" --new-datadir "$NEWDATA" \
+          --old-bindir $OLDBIN --new-bindir $NEWBIN \
+          "$@"
+      ''
+    )
   ];
 
   ## nix
